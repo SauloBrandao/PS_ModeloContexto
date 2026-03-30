ad8c4ab4cacd7173bf6404cbded2265955b179f9
SauloBrandao <2023211510080@iesp.edu.br>
2026-03-30 05:27:48 -0300

    criando base

---
+++ src/main/java/io/github/modelocontexto/modelocontextoapi/ModelocontextoapiApplication.java
@@ -0,0 +1,13 @@
+package io.github.modelocontexto.modelocontextoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelocontextoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelocontextoapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/modelocontexto/modelocontextoapi/ModelocontextoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.modelocontexto.modelocontextoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelocontextoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
